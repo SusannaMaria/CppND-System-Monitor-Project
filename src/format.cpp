5c46b8367c50195c462fc3b523b886df49422228
Susanna <susanna@olsoni.de>
2020-06-21 10:23:39 +0200

    doxygen format

--- src/format.cpp
+++ src/format.cpp
@@ -8,14 +8,15 @@ using namespace std;
 
 using std::string;
 
-//     time_t my_time = time(&utime_sec); 
-// char buff[20];
-// strftime(buff, 20, "%H:%M:%S", localtime(&my_time));
-
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
+/**
+ * Return time as string in HH:MM:SS format
+ *
+ * Because uptime of system is likely more than 1d, I decided to extend the
+ * return string if day > 0, it is added as prefix to the HH:MM:SS
+ *
+ * @param uptime TIme in seconds
+ * @return string Used in function in NCursesDisplay namespace
+ */
 string Format::ElapsedTime(long uptime) {
   int day = uptime / (24 * 3600);
 
@@ -30,25 +31,35 @@ string Format::ElapsedTime(long uptime) {
 
   std::stringstream ss;
 
-  if (day>0){
-
-  
-  ss << day << " day";
-  if (day > 1) {
-    ss << "s";
-  }
-  ss << ", ";
+  if (day > 0) {
+    ss << day << " day";
+    if (day > 1) {
+      ss << "s";
+    }
+    ss << ", ";
   }
 
-  ss << setfill('0') << setw(2) << hour << ":" << setfill('0')
-     << setw(2) << minutes << ":" << setfill('0') << setw(2) << seconds;
+  ss << setfill('0') << setw(2) << hour << ":" << setfill('0') << setw(2)
+     << minutes << ":" << setfill('0') << setw(2) << seconds;
 
   return ss.str();
 }
 
-std::string Format::padTo(const std::string str, const size_t num, const char paddingChar)
-{
+/**
+ * Pad string to specific size with specific character
+ *
+ * Because of display problem of User and commandline with various length of
+ * string, I decided to provide function which returns string which has defined
+ * length
+ *
+ * @param str String which will be extended or shorten to reach size
+ * @param num Size of return string
+ * @param paddingChar Fill out string to required size with character
+ * @return std::string Used in function in NCursesDisplay namespace
+ */
+std::string Format::padTo(const std::string str, const size_t num,
+                          const char paddingChar = ' ') {
   std::string result = str;
-  result.resize (num, paddingChar);
+  result.resize(num, paddingChar);
   return result;
 }
